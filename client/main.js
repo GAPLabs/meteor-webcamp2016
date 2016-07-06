57499ae8fbe49746db0972bef9e509b296222a55
Kurt Obispo <kurtobispo@gmail.com>
2016-07-06 17:32:28 +0800

    Helpers

---
+++ client/main.js
@@ -0,0 +1,12 @@
+Template.messageList.helpers({
+  messages: [
+    {
+      username: 'septa_unella',
+      text: 'Shame. Shame. Shame.'
+    },
+    {
+      username: 'hodor',
+      text: 'Hold the door'
+    }
+  ]
+});
