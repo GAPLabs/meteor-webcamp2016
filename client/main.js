f80e3239b909088e05cf702bdb73a478225519df
Kurt Obispo <kurtobispo@gmail.com>
2016-07-06 18:45:07 +0800

    User Accounts

--- client/main.js
+++ client/main.js
@@ -4,16 +4,30 @@ Template.messageList.helpers({
   }
 });
 
+Template.message.helpers({
+  isMyMessage() {
+    if (Meteor.user() && Meteor.user().username == this.username) {
+      return "mine";
+    } else {
+      return "";
+    }
+  }
+});
+
 Template.messageForm.events({
   "submit #message-form"(event) {
     event.preventDefault();
 
     Messages.insert({
-      username: "john_snow",
+      username: Meteor.user().username,
       text: event.target.messageBox.value,
       createdAt: new Date()
     });
 
     event.target.messageBox.value = "";
   }
+});
+
+Accounts.ui.config({
+  passwordSignupFields: "USERNAME_ONLY"
 });
