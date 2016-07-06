f50079796e0c93bbd98f66c6f56c5a7038e6cf31
Kurt Obispo <kurtobispo@gmail.com>
2016-07-06 17:52:52 +0800

    Forms and Events

--- client/main.js
+++ client/main.js
@@ -3,3 +3,17 @@ Template.messageList.helpers({
     return Messages.find();
   }
 });
+
+Template.messageForm.events({
+  "submit #message-form"(event) {
+    event.preventDefault();
+
+    Messages.insert({
+      username: "john_snow",
+      text: event.target.messageBox.value,
+      createdAt: new Date()
+    });
+
+    event.target.messageBox.value = "";
+  }
+});
